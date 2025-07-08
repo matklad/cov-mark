7b8086173e289579341919aa04265c9721a95435
c <c@farsight.net>
2025-07-08 16:14:53 +0200

    feat: output mark name in error message

--- src/lib.rs
+++ src/lib.rs
@@ -222,11 +222,12 @@ pub mod __rt {
             match last.expected_hits {
                 Some(hits) => assert!(
                     hit_count == hits,
-                    "mark was hit {} times, expected {}",
+                    "{} mark was hit {} times, expected {}",
+                    self.inner.mark,
                     hit_count,
                     hits
                 ),
-                None => assert!(hit_count > 0, "mark was not hit"),
+                None => assert!(hit_count > 0, "{} mark was not hit", self.inner.mark),
             }
         }
     }
